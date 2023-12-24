cc13331fe99019024c149f6534dba45514f11a0b
limFakson <fakeyejoshua2005@gmail.com>
2023-12-24 17:46:03 +0100

    insight

---
+++ Script/carousel.js
@@ -0,0 +1,5 @@
+const gallery = document.querySelector(".gallery");
+const immageGallery = document.querySelector(".image-gallery");
+const galleryitems = document.querySelectorAll(".gallery-item");
+const controlprevious = document.querySelector(".previous");
+const controlnext = document.querySelector(".next");
