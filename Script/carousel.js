6cd45e8766667434a4f39bd02d76e9758b611e67
limFakson <fakeyejoshua2005@gmail.com>
2023-12-26 15:40:45 +0100

    insight

--- Script/carousel.js
+++ Script/carousel.js
@@ -1,3 +1,47 @@
+// Changing the logo pics when resolution hits 1014 width
+function checkResolution(){
+    const logo = document.querySelector('.logo img');
+
+    const current_width = window.innerWidth;
+    
+    desired_width = 1014;
+
+    if (current_width <= desired_width){
+        console.log('Resolution chaged');
+        logo.src = "../src/files/Logo-dark.png";
+        console.log('image chagned');
+    }
+
+    //For the menu slide down and up
+    const openMenu = document.querySelector('.openmenu');
+    const closeMenu = document.querySelector('.closemenu');
+    const menu = document.querySelector('.flex');
+
+    openMenu.addEventListener('click', function() {
+        function opener(){
+            menu.style.top = '0';
+        };
+
+        opener();
+    });
+
+    closeMenu.addEventListener('click', function() {
+        function opener(){
+            
+            menu.style.top = '-400px';
+        };
+
+        opener();
+    });
+
+
+}
+
+window.addEventListener('resize', checkResolution);
+
+checkResolution()
+
+
 const gallery = document.querySelector(".gallery");
 const immageGallery = document.querySelector(".image-gallery");
 const galleryitems = document.querySelectorAll(".gallery-item");
