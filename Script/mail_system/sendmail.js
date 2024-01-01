d49a1436449d246bc9f88f364cc477378ad966e4
limFakson <fakeyejoshua2005@gmail.com>
2024-01-01 14:19:24 +0100

    insight

--- Script/mail_system/sendmail.js
+++ Script/mail_system/sendmail.js
@@ -2,12 +2,51 @@ function footerContentLoaded() {
     const form = document.querySelector('.subcription_mail');
     console.log('mail section is working')
 
-    form.addEventListener('submit', (e)=>{
-        var inputed_email = document.getElementById('email_input').value;
-        e.preventDefault()
+    function sendEmail() {
+        // Get values from the form
+        const recipientEmail = document.getElementById('email_input').value;
+        
 
-        console.log(inputed_email);
-        inputed_email = " ";
-    });
+        emailjs.init("XhfGOvYII8Y42T2jb");
+
+        console.log(recipientEmail);
+
+
+        // EmailJs send function
+        emailjs.send("service_x64z35o", "template_yxuiv1g", {
+            to_email: recipientEmail,
+            message: "Thank you for subscribing to our newletter"
+        })
+
+
+        .then(
+            function(response) {
+        
+                console.log("Email sent successfully", response);
+        
+        
+                // Add any success handling code here
+        
+        
+            },
+        
+        
+            function(error) {
+        
+        
+                console.log("Email sending failed", error);
+        
+                // Add any error handling code here
+        
+        
+            }
+        
+
+        );
+
+
+    };
+
+
 
 }
